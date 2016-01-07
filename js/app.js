5ce2f44cc00009877fd1540904a007aa994d9d61
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-07 08:07:37 -0800

    projects data updated with details and dates. functions updated with the correct variable names. Testing functionality now.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 var portfolioItems = [];
 
-function Projects (project) {
+function Project (project) {
   this.title = project.title;
   this.datePublished = project.datePublished;
-  this.details = project.details.
+  this.details = project.details;
+  this.backgroundImg = project.backgroundImg;
+  this.link = project.link;
 }
 
-Projects.prototype.toHtml = function() {
+Project.prototype.toHtml = function() {
   var $newProject = $('article.template').clone();
   $newProject.data('title', this.title);
   $newProject.data('datePublished', this.datePublished);
   $newProject.data('details', this.details);
+  $newProject.data('backgroundImg', this.backgroundImg);
+  $newProject.data('link', this.link);
 
   $newProject.removeClass('template');
   return newProject;
 }
 
-// portfolioItems.sort(function(a,b) {
-//   return (new Date(b.datePublished)) - (new Date(a.datePublished));
-// });
+projectsList.sort(function(a,b) {
+  return (new Date(b.datePublished)) - (new Date(a.datePublished));
+});
 
-// ADDPROJECTFILESHERE.forEach(function(ele) {
-//   portfolioItems.push(new Article(ele));
-// });
+projectsList.forEach(function(ele) {
+  portfolioItems.push(new Project(ele));
+});
 
-// portfolioItems.forEach(function(a){
-//   $('#portfolio').append(a.toHtml())
-// });
+portfolioItems.forEach(function(a){
+  $('#portfolio').append(a.toHtml())
+});
--- js/projects.js
+++ js/projects.js
@@ -1,30 +1,30 @@
-var projects = [
+var projectsList = [
   {
     title: 'Note Fellows',
-    datePublished: '',
+    datePublished: '2015-11-25',
     details: 'Distraction free simple note taking. So you can focus on what is important.',
-    backgroundImg: 'screenshot',
+    backgroundImg: '#',
     link: '<a href="http://clee46.github.io/note-fellows/"></a>'
   },
   {
     title: 'Vote Tracker',
-    datePublished: '',
+    datePublished: '2015-11-15',
     details: '<p>Vote for the place you would most like to visit!</p>',
-    backgroundImg: 'paris',
+    backgroundImg: '#',
     link: '<a href="https://github.com/jaredscarr/votetracker-jared"></a>'
   },
   {
     title: "Snookie's Cookies",
-    datePublished: '',
+    datePublished: '2015-11-10',
     details: '<p>Salmon cookies made with fish, salmon cookies are delish!</p>',
     backgroundImg: 'salmon',
     link: '<a href="https://github.com/jaredscarr/cookie-standjared"></a>'
   },
   {
     title: 'Guessing Game',
-    datePublished: '',
+    datePublished: '2015-11-05',
     details: '',
-    backgroundImg: '',
+    backgroundImg: '#',
     link: '<a href="https://github.com/jaredscarr/guessing-game"></a>'
   }
 ];
