4f1318ee7743a894c05d93e323b7035b70d5cef0
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-06 16:43:50 -0800

    added class to nav bar. added project file with an array of objects

---
+++ js/projects.js
@@ -0,0 +1,30 @@
+var projects = [
+  {
+    title: 'Note Fellows',
+    datePublished: '',
+    details: 'Distraction free simple note taking. So you can focus on what is important.',
+    backgroundImg: 'screenshot',
+    link: '<a href="http://clee46.github.io/note-fellows/"></a>'
+  },
+  {
+    title: 'Vote Tracker',
+    datePublished: '',
+    details: '<p>Vote for the place you would most like to visit!</p>',
+    backgroundImg: 'paris',
+    link: '<a href="https://github.com/jaredscarr/votetracker-jared"></a>'
+  },
+  {
+    title: "Snookie's Cookies",
+    datePublished: '',
+    details: '<p>Salmon cookies made with fish, salmon cookies are delish!</p>',
+    backgroundImg: 'salmon',
+    link: '<a href="https://github.com/jaredscarr/cookie-standjared"></a>'
+  },
+  {
+    title: 'Guessing Game',
+    datePublished: '',
+    details: '',
+    backgroundImg: '',
+    link: '<a href="https://github.com/jaredscarr/guessing-game"></a>'
+  }
+];
