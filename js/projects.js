827a679316e2fd2ecf6fabe605650381d919d36f
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-08 22:29:51 -0800

    added images to objects in project.js. Some css experimentation but nothing was really kept.

--- js/projects.js
+++ js/projects.js
@@ -3,28 +3,28 @@ var projectsList = [
     title: 'Note Fellows',
     datePublished: '2015-11-25',
     details: 'Distraction free note taking. So you can focus on what is important.',
-    image: '#',
+    image: '<img src="img/notefellows.png" alt="Note Fellows" />',
     link: '<a href="http://clee46.github.io/note-fellows/">NoteFellows</a>'
   },
   {
     title: 'Vote Tracker',
     datePublished: '2015-11-15',
     details: 'Vote for the place you would most like to visit!',
-    image: '#',
+    image: '<img src="img/voteTracker.png" alt="Which One Would You Choose?" />',
     link: '<a href="https://github.com/jaredscarr/votetracker-jared"></a>'
   },
   {
     title: 'Snookie\'s Cookies',
     datePublished: '2015-11-10',
     details: 'Salmon cookies made with fish, salmon cookies are delish!',
-    image: 'salmon',
+    image: '<img src="img/salmonCookies.png" alt="Salmon Cookies" />',
     link: '<a href="https://github.com/jaredscarr/cookie-standjared"></a>'
   },
   {
     title: 'Guessing Game',
     datePublished: '2015-11-05',
     details: '',
-    image: '#',
+    image: '<img src="img/guessingGame.png" alt="Guessing Game" />',
     link: '<a href="https://github.com/jaredscarr/guessing-game"></a>'
   }
 ];
